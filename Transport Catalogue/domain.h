563d4f8e7e60f4889ae327e2c986737254cb0ff1
77Kimi77 <155003806+77Kimi77@users.noreply.github.com>
2025-02-17 17:58:46 +0300

    Update domain.h

--- Transport Catalogue/domain.h
+++ Transport Catalogue/domain.h
@@ -5,22 +5,29 @@
 #include <unordered_map>
 #include "geo.h"
 namespace transport {
+    /*Фаил содержит различные структуры, которые используются
+    для описания данных которые хранятся в хэш-таблицах в классе транспортного каталога
+    
+    */
     struct Stop {
-        std::string name_;
-        geo::Coordinates coordinates_;
-        std::set<std::string> buses_by_stop_;
+        std::string name_; //название остановки
+        geo::Coordinates coordinates_; //координаты(долгота, широта) остановки
+        std::set<std::string> buses_by_stop_; //контейнер для зранения всех автобусов проходящих чернз определенную остановку
     };
 
     struct Bus {
-        std::string name_;
-        std::vector<const Stop*> stops_;
-        bool is_circle_;
+        std::string name_; //номер* маршрута
+        std::vector<const Stop*> stops_; //вектор структур остановок для данного маршрута
+        bool is_circle_; //индикатор не-/кругового маршрута
     };
 
     struct BusStat {
-        size_t stops_count_;
-        size_t unique_stops_count_;
-        double route_length_;
-        double curvature_;
+    /* Структура, которая описывает характеристики определенного маршрута,
+    согласно указанным остановкам;
+    */
+        size_t stops_count_; //кол-во остановок
+        size_t unique_stops_count_; // кол-во уникальных остановок
+        double route_length_; //длина маршрута 
+        double curvature_; //фактическая длина маршрута по кривой
     };
 }
